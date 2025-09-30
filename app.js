3dc729c9dec2a8dde0761ec1f00085be7dab98c9
dhruv1067 <dhruv67d@gmail.com>
2025-09-30 13:19:52 +0530

    changes in html and app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const base_url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(",msg");
+
